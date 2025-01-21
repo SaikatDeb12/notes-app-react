3557625eb35e7c132a91ab84d8d7b59bad8a79e7
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-21 13:53:22 +0530

    color-list

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import "./sidebar.css";
 import { IoMdAddCircle } from "react-icons/io";
 function Sidebar() {
+  const colors = ["#fe9b72", "#fec971", "#00d4fe", "#b693fd", "#e4ee91"];
+
   return (
     <div className="sidebar">
       <IoMdAddCircle className="addIcon" />
-      <div className="colors">
-        <p className="color"></p>
-        <p className="color"></p>
-        <p className="color"></p>
-      </div>
+      <ul className="colors-list">
+        {colors.map((item, id) => {
+          return (
+            <li
+              className="color"
+              key={id}
+              style={{ backgroundColor: item }}
+            ></li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
