224f625e226288afdbaf5ce4844b337da6511714
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-21 13:14:59 +0530

    adjustment, add notes button

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./sidebar.css";
+import { IoMdAddCircle } from "react-icons/io";
 function Sidebar() {
-  return <div className="sidebar">Choose</div>;
+  return (
+    <div className="sidebar">
+      <IoMdAddCircle className="addIcon" />
+      <div className="colors">
+        <p className="color"></p>
+        <p className="color"></p>
+        <p className="color"></p>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
