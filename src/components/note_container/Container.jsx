f4573bb2f372248369a2e60af096985d24198a80
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-22 13:02:10 +0530

    minor changes

--- src/components/note_container/Container.jsx
+++ src/components/note_container/Container.jsx
@@ -17,17 +17,19 @@ function Container({ notes, deleteNote, updateNotes }) {
     <div className="notes-container custom-scroll">
       <h1>Notes</h1>
       <div className="notesList">
-        {newNotes.length == 0
-          ? "No notes to show"
-          : newNotes.map((item) => (
-              //each note getting unique key.. from (notes.id)
-              <Note
-                data={item}
-                key={item.id}
-                deleteNote={deleteNote}
-                updateNotes={updateNotes}
-              />
-            ))}
+        {newNotes.length == 0 ? (
+          <p className="no-notes">Add some notes :)</p>
+        ) : (
+          newNotes.map((item) => (
+            //each note getting unique key.. from (notes.id)
+            <Note
+              data={item}
+              key={item.id}
+              deleteNote={deleteNote}
+              updateNotes={updateNotes}
+            />
+          ))
+        )}
       </div>
     </div>
   );
